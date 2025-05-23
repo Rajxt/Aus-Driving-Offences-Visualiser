31da376b6235ab58ce4c5aee5dd438dffdcb969f
Ivan Wilson Setiabudi <ivanwilsonsetiabudi@gmail.com>
2025-05-23 17:14:48 +1000

    test

--- js/LoadData.js
+++ js/LoadData.js
@@ -15,7 +15,7 @@ export async function loadLine() {
 
 // LoadData.js
 export async function loadBar() {
-    const data = await d3.csv("AgewithMonth.csv", d => ({
+    const data = await d3.csv("/data/AgewithMonth.csv", d => ({
         month: d.START_DATE.slice(0, 7), // "2023-01"
         ageGroup: d.AGE_GROUP,
         fines: +d.FINES,
